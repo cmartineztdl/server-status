c95d4f3d3810f4cfb86d37315c78857b195ba0cd
Carlos Martinez Gonzalez <c.martinez.tdl@gmail.com>
2023-09-27 15:29:45 +0200

    Adds server status in request response

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
 import { createServer, Server, IncomingMessage, ServerResponse } from 'http';
+import { totalmem, freemem, cpuCount, cpuUsage } from 'os-utils';
 import chalk from 'chalk';
 
-const helloWorld = 'Hello World!';
 const port = 3001;
 
 const server: Server = createServer(
   (req: IncomingMessage, res: ServerResponse) => {
     console.log(`${chalk.bgBlue(req.method)} - ${chalk.blue(req.url)}`);
 
-    res.end(JSON.stringify({ data: helloWorld }));
+    cpuUsage(cpusUsagePercenge => {
+      res.end(
+        JSON.stringify({
+          totalmem: totalmem(),
+          freemem: freemem(),
+          cpuCount: cpuCount(),
+          cpusUsagePercenge,
+        })
+      );
+    });
   }
 );
 
