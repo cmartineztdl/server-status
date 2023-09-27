f3ba9cab63c64eef11d9f3b1842ee3be38b1eb53
Carlos Martinez Gonzalez <c.martinez.tdl@gmail.com>
2023-09-27 15:29:27 +0200

    Adds coloured logs

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import { createServer, Server, IncomingMessage, ServerResponse } from 'http';
+import chalk from 'chalk';
 
 const helloWorld = 'Hello World!';
 const port = 3001;
 
 const server: Server = createServer(
   (req: IncomingMessage, res: ServerResponse) => {
+    console.log(`${chalk.bgBlue(req.method)} - ${chalk.blue(req.url)}`);
+
     res.end(JSON.stringify({ data: helloWorld }));
   }
 );
 
-server.listen(port, () => console.log(`Server listening at port ${port}`));
+server.listen(port, () =>
+  console.log(chalk.black.bgGreen(`Server listening at port ${port}`))
+);
