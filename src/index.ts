16b10e3ed016a2b9a0dc26f05ec410bb131558b2
Carlos Martinez Gonzalez <c.martinez.tdl@gmail.com>
2023-09-27 15:28:57 +0200

    Creates the server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-const helloWorld = 'Hello World!';
+import { createServer, Server, IncomingMessage, ServerResponse } from 'http';
 
-console.log(helloWorld);
+const helloWorld = 'Hello World!';
+const port = 3001;
+
+const server: Server = createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
+    res.end(JSON.stringify({ data: helloWorld }));
+  }
+);
+
+server.listen(port, () => console.log(`Server listening at port ${port}`));
