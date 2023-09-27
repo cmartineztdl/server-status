d1f7084ec49dd4c0b69cce68b680fae054c0f993
Carlos Martinez Gonzalez <c.martinez.tdl@gmail.com>
2023-09-27 15:30:04 +0200

    Writes logs in a file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { createServer, Server, IncomingMessage, ServerResponse } from 'http';
+import { appendFile } from 'fs';
 import { totalmem, freemem, cpuCount, cpuUsage } from 'os-utils';
 import chalk from 'chalk';
 
@@ -7,6 +8,8 @@ const port = 3001;
 const server: Server = createServer(
   (req: IncomingMessage, res: ServerResponse) => {
     console.log(`${chalk.bgBlue(req.method)} - ${chalk.blue(req.url)}`);
+
+    addLogToLoFile(`${req.method} - ${req.url}`);
 
     cpuUsage(cpusUsagePercenge => {
       res.end(
@@ -24,3 +27,13 @@ const server: Server = createServer(
 server.listen(port, () =>
   console.log(chalk.black.bgGreen(`Server listening at port ${port}`))
 );
+
+function addLogToLoFile(message: string) {
+  const file = 'server.log';
+
+  appendFile(file, `${new Date().toISOString()}: ${message}\n`, err => {
+    if (err) {
+      console.log(chalk.red(`Ups! ${err.message}`));
+    }
+  });
+}
